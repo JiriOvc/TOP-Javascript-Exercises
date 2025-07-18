2a179da2548bf9a0719cfc789f715e2730293586
Yir-Ca <64151453+JiriOvc@users.noreply.github.com>
2025-07-18 17:17:07 +0200

    added function to create grid of squares

--- etch-a-sketch/etch.js
+++ etch-a-sketch/etch.js
@@ -1,1 +1,19 @@
-console.log("Hello")
+let gridContainer = document.querySelector("#grid-container");
+let square = document.createElement("div");
+let squareNum = 5;
+
+let squareSize = gridContainer.clientWidth / squareNum;
+
+for (let i = 1; i <= squareNum * squareNum; i++) {
+    let gridContainer = document.querySelector("#grid-container");
+    let square = document.createElement("div");
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+    square.style.width = Math.floor(squareSize) + "px";
+    square.style.height = Math.floor(squareSize) + "px";
+}
+
+console.log(gridContainer.clientWidth)
+console.log(gridContainer.clientHeight)
+console.log(squareSize);
+
