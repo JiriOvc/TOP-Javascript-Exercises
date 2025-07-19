18f8c994e5accc0e4113ea243a5b3efab9336548
Yir-Ca <64151453+JiriOvc@users.noreply.github.com>
2025-07-19 14:48:45 +0200

    createGrid update
    createGrid function creates the number of squares correctly the first time it is run. Subsequent tries do not change the grid size.

--- etch-a-sketch/etch.js
+++ etch-a-sketch/etch.js
@@ -8,26 +8,31 @@ let squareNum = 30;
 let gridSizeBtn = document.querySelector(".grid-size")
 
 function getGridSize() {
-    return squareNum = prompt("Set number of squares in top row. Ideally between 10 - 40")
+    return squareNum = prompt("Set number of squares in top row. Ideally between 30 - 50");
 }
 
-// Calculate Square Size
-let squareSize = gridContainer.clientWidth / squareNum;
+gridSizeBtn.addEventListener("click", createGrid);
 
 // Create Grid
-for (let i = 1; i <= squareNum * squareNum; i++) {
+function createGrid() {
+    squareNum = getGridSize();
+
+    // Calculate Square Size
+    let squareSize = gridContainer.clientWidth / squareNum;
+
+    for (let i = 1; i <= squareNum * squareNum; i++) {
     let gridContainer = document.querySelector("#grid-container");
     let square = document.createElement("div");
     square.classList.add("square");
     gridContainer.appendChild(square);
     square.style.width = Math.floor(squareSize) + "px";
     square.style.height = Math.floor(squareSize) + "px";
+    };
+
 }
 
 // Change Square Color on Mouse Enter
 
 
 // Erase Grid
-
-getGridSize()
 console.log(squareNum)
