f3c2a451469e8bb7722cfdab3af89966e6c7f426
Yir-Ca <64151453+JiriOvc@users.noreply.github.com>
2025-07-20 10:49:10 +0200

    added change color effect and erase button

--- etch-a-sketch/etch.js
+++ etch-a-sketch/etch.js
@@ -2,19 +2,25 @@
 // Default Grid Size
 let gridContainer = document.querySelector("#grid-container");
 let square = document.createElement("div");
-let squareNum = 30;
+let squareNum = 80;
 
-// Change Grid Size Button
-let gridSizeBtn = document.querySelector(".grid-size")
+// Calculate Square Size
+let squareSize = gridContainer.clientWidth / squareNum;
 
-gridSizeBtn.addEventListener("click", () => {
-    createGrid();
-})
+// Create Default Grid
+for (let i = 1; i <= squareNum * squareNum; i++) {
+    let gridContainer = document.querySelector("#grid-container");
+    let square = document.createElement("div");
+    square.classList.add("square");
+    gridContainer.appendChild(square);
+    square.style.width = Math.floor(squareSize) + "px";
+    square.style.height = Math.floor(squareSize) + "px";
+};
 
-// Create Grid
+// Change Grid Size
 function createGrid(squareNum) {
     gridContainer.innerHTML = "";
-    squareNum = prompt("Set number of squares in top row. Ideally between 30 - 50");
+    squareNum = prompt("Set number of squares in top row. Ideally between 70 and 120");
 
     // Calculate Square Size
     let squareSize = gridContainer.clientWidth / squareNum;
@@ -28,13 +34,42 @@ function createGrid(squareNum) {
     square.style.height = Math.floor(squareSize) + "px";
     };
 
+    // Change Square Color on Mouse Enter
+    let squareSelect = document.querySelectorAll(".square")
+    squareSelect.forEach(element => {
+        element.addEventListener("mouseenter", () => {
+            element.style.backgroundColor = "darkseagreen"
+        })
+    })
+
+    // Erase Grid
+    let eraseBtn = document.querySelector(".erase")
+    eraseBtn.addEventListener("click", () => {
+    squareSelect.forEach(element => {
+        element.style.backgroundColor = "lightgray"
+    })
+})
+
 }
 
-
-
-
-
 // Change Square Color on Mouse Enter
+let squareSelect = document.querySelectorAll(".square")
+squareSelect.forEach(element => {
+        element.addEventListener("mouseenter", () => {
+            element.style.backgroundColor = "darkseagreen"
+        })
+    })
 
+// Change Grid Size Button
+let gridSizeBtn = document.querySelector(".grid-size")
+gridSizeBtn.addEventListener("click", () => {
+    createGrid();
+})
 
 // Erase Grid
+let eraseBtn = document.querySelector(".erase")
+eraseBtn.addEventListener("click", () => {
+    squareSelect.forEach(element => {
+        element.style.backgroundColor = "lightgray"
+    })
+})
