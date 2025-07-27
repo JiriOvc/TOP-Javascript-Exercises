a04fe79c7732b54adc18f9c7780191f93663c5ab
Yir-Ca <64151453+JiriOvc@users.noreply.github.com>
2025-07-27 15:41:25 +0200

    Calculator script update
    Created and tested the 4 basic operations (add, subtract, multiply and divide).

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+// Operations
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+console.log(add(9, 3))
+console.log(subtract(9, 3))
+console.log(multiply(9, 3))
+console.log(divide(9, 3))
