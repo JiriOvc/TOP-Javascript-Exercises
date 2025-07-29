2d18dfde30bde37691b859ed4dcd444888b5b3e8
Yir-Ca <64151453+JiriOvc@users.noreply.github.com>
2025-07-29 11:16:45 +0200

    Script update
    Buttons 0-9, +-*/, equal, clear
    - all have their event listeners now

--- calculator/script.js
+++ calculator/script.js
@@ -45,6 +45,15 @@ let displayContent = display.textContent;
 display.textContent = "";
 
 // Number buttons
+let btnZero = document.querySelector(".btn-zero");
+btnZero.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 0;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
 let btnOne = document.querySelector(".btn-one");
 btnOne.addEventListener("click", () => {
     let number = document.createElement("div")
@@ -58,6 +67,69 @@ let btnTwo = document.querySelector(".btn-two");
 btnTwo.addEventListener("click", () => {
     let number = document.createElement("div")
     number.textContent = 2;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnThree = document.querySelector(".btn-three");
+btnThree.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 3;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnFour = document.querySelector(".btn-four");
+btnFour.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 4;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnFive = document.querySelector(".btn-five");
+btnFive.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 5;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnSix = document.querySelector(".btn-six");
+btnSix.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 6;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnSeven = document.querySelector(".btn-seven");
+btnSeven.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 7;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnEight = document.querySelector(".btn-eight");
+btnEight.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 8;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnNine = document.querySelector(".btn-nine");
+btnNine.addEventListener("click", () => {
+    let number = document.createElement("div")
+    number.textContent = 9;
     display.appendChild(number)
     displayContent = display.textContent
     console.log(displayContent)
@@ -83,7 +155,7 @@ btnPlus.addEventListener("click", () => {
 
 let btnMinus = document.querySelector(".btn-minus");
 btnMinus.addEventListener("click", () => {
-     displayContentSplit = displayContent.split(" ");
+    displayContentSplit = displayContent.split(" ");
     if (displayContentSplit.length === 3) {
         operandA = parseInt(displayContentSplit[0], 10);
         operator = displayContentSplit[1]
@@ -92,6 +164,38 @@ btnMinus.addEventListener("click", () => {
     }
     let number = document.createElement("div")
     number.textContent = ` - `;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnMultiply = document.querySelector(".btn-multiply");
+btnMultiply.addEventListener("click", () => {
+    displayContentSplit = displayContent.split(" ");
+    if (displayContentSplit.length === 3) {
+        operandA = parseInt(displayContentSplit[0], 10);
+        operator = displayContentSplit[1]
+        operandB = parseInt(displayContentSplit[2], 10);
+        display.textContent = operate(operandA, operator, operandB)
+    }
+    let number = document.createElement("div")
+    number.textContent = ` * `;
+    display.appendChild(number)
+    displayContent = display.textContent
+    console.log(displayContent)
+});
+
+let btnDivide = document.querySelector(".btn-divide");
+btnDivide.addEventListener("click", () => {
+    displayContentSplit = displayContent.split(" ");
+    if (displayContentSplit.length === 3) {
+        operandA = parseInt(displayContentSplit[0], 10);
+        operator = displayContentSplit[1]
+        operandB = parseInt(displayContentSplit[2], 10);
+        display.textContent = operate(operandA, operator, operandB)
+    }
+    let number = document.createElement("div")
+    number.textContent = ` / `;
     display.appendChild(number)
     displayContent = display.textContent
     console.log(displayContent)
@@ -115,6 +219,6 @@ btnEqual.addEventListener("click", () => {
     console.log(`${typeof operandA} ${operator} ${typeof operandB}`)
     console.log(operate(operandA, operator, operandB))
     display.textContent = operate(operandA, operator, operandB)
+    console.log(typeof displayContent)
 });
 
-console.log(displayContent)
