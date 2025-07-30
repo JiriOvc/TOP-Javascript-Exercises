439bcda77ea1748fa6790b95b8b500763130a789
Yir-Ca <64151453+JiriOvc@users.noreply.github.com>
2025-07-30 11:36:13 +0200

    Script update
    checkForResult()
    - after clicking equal button the next click on digit shows just that number, it no longer adds the number to the result on display

--- calculator/script.js
+++ calculator/script.js
@@ -48,51 +48,61 @@ display.textContent = "";
 // Digit buttons
 let btnZero = document.querySelector(".btn-zero");
 btnZero.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(0)
 });
 
 let btnOne = document.querySelector(".btn-one");
 btnOne.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(1)
 });
 
 let btnTwo = document.querySelector(".btn-two");
 btnTwo.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(2)
 });
 
 let btnThree = document.querySelector(".btn-three");
 btnThree.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(3)
 });
 
 let btnFour = document.querySelector(".btn-four");
 btnFour.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(4)
 });
 
 let btnFive = document.querySelector(".btn-five");
 btnFive.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(5)
 });
 
 let btnSix = document.querySelector(".btn-six");
 btnSix.addEventListener("click", () => {
+    checkForResult
     displayDigitOrOperator(6)
 });
 
 let btnSeven = document.querySelector(".btn-seven");
 btnSeven.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(7)
 });
 
 let btnEight = document.querySelector(".btn-eight");
 btnEight.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(8)
 });
 
 let btnNine = document.querySelector(".btn-nine");
 btnNine.addEventListener("click", () => {
+    checkForResult()
     displayDigitOrOperator(9)
 });
 
@@ -139,17 +149,18 @@ btnDivide.addEventListener("click", () => {
 let btnClear = document.querySelector(".btn-clear");
 btnClear.addEventListener("click", () => {
     display.textContent = "";
+    displayContentSplit = "";
 });
 
 let btnEqual = document.querySelector(".btn-equal");
 btnEqual.addEventListener("click", () => {
     calculate();
+    displayContentSplit = " ";
 });
 
 let btnPoint = document.querySelector(".btn-point");
 btnPoint.addEventListener("click", () => {
     displayDigitOrOperator(".")
-    
 });
 
 // Supporting Functions
@@ -160,7 +171,11 @@ function calculate() {
     operator = displayContentSplit[1]
     operandB = parseFloat(displayContentSplit[2], 10);
 
-    if (operandB === 0 && operator === "/") {
+    if (!operator) {
+        return display.textContent = operandA
+    } else if (!operandB) {
+        return display.textContent = `Error`
+    } else if (operandB === 0 && operator === "/") {
         display.textContent = "Critical hit!"
     } else {
         display.textContent = operate(operandA, operator, operandB).toFixed(5).replace(/\.?0+$/, '');
@@ -180,4 +195,10 @@ function displayDigitOrOperator(digitOrOperator) {
     display.appendChild(number)
     displayContent = display.textContent
     console.log(displayContent)
+}
+
+function checkForResult() {
+    if (displayContentSplit === " ") {
+        display.textContent = ""
+    }
 }
